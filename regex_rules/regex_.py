2e94ea03dd1584f255f9b7996902b0ffd338a001
hailleenvarelag <hailleen.gonzalez@taxall.com.br>
2025-03-27 10:30:44 -0300

    Modificando regex 27/03

--- regex_rules/regex_.py
+++ regex_rules/regex_.py
@@ -223,15 +223,15 @@ class RegexRules():
                 # 'valor_total_origem_credito': r"Valor Total\s*([\d.,]+)",
 
                 # Origem do Crédito
-                'periodo_apuracao_origem_credito': r"ORIGEM DO CRÉDITO.*?(\d{2}/\d{2}/\d{4})\s+Período de Apuração1",
-                'cnpj_pagamento_origem_credito': r"ORIGEM DO CRÉDITO.*?(\d{2}\.\d{3}\.\d{3}/\d{4}-\d{2})\s+CNPJ do Pagamento",
-                'codigo_receita_origem_credito': r"ORIGEM DO CRÉDITO.*?Código da Receita\s+(\d{4})",
-                'grupo_tributo_origem_credito': r"ORIGEM DO CRÉDITO.*?Grupo de Tributo\s+([A-Z]+(?:/[A-Z]+)?(?:,\s[A-Z]+)*)",
-                'data_arrecadacao_origem_credito': r"ORIGEM DO CRÉDITO.*?Data da Arrecadação\s+(\d{2}/\d{2}/\d{4})",
-                'valor_principal_origem_credito': r"ORIGEM DO CRÉDITO.*?Valor do Principal\s+([\d.,]+)",
-                'valor_multa_origem_credito': r"ORIGEM DO CRÉDITO.*?Valor da Multa\s+([\d.,]+)",   
-                'valor_juros_origem_credito': r"ORIGEM DO CRÉDITO.*?Valor dos Juros\s+([\d.,]+)",
-                'valor_total_origem_credito': r"ORIGEM DO CRÉDITO.*?Valor Total\s+([\d.,]+)",
+                'periodo_apuracao_origem_credito': r"ORIGEM DO CRÉDITO\s+(\d{2}/\d{2}/\d{4})\s+Período de Apuração\d*\.",
+                'cnpj_origem_credito': r"(\d{2}\.\d{3}\.\d{3}/\d{4}-\d{2})\s+CNPJ do Pagamento",
+                'codigo_receita_origem_credito': r"Código da Receita\s+(\d{4})",
+                'grupo_tributo_origem_credito': r"Grupo de Tributo\s+([A-Z]+)(?=Código)",
+                'data_arrecadacao_origem_credito': r"Data de Arrecadação\s+(\d{2}/\d{2}/\d{4})",  # Corrigi "da" para "de"
+                'valor_principal_origem_credito': r"Valor do Principal\s+([\d.,]+)",
+                'valor_multa_origem_credito': r"Valor da Multa\s+([\d.,]+)",   
+                'valor_juros_origem_credito': r"([\d.,]+)\s+Valor dos Juros",  # Capture o valor ANTES do rótulo
+                'valor_total_origem_credito': r"Valor Total\s+([\d.,]+)\b",  # \b evita capturar o CNPJ
 
                 #DARF
                 'periodo_apuracao_darf': r"Período de Apuração\s*([\d/]+)\s",
@@ -248,6 +248,19 @@ class RegexRules():
 
                 #GPS
 
+            },
+
+            3: {
+                # Origem do Crédito
+                'periodo_apuracao_origem_credito': r"ORIGEM DO CRÉDITO.*?(\d{2}/\d{2}/\d{4})\s*+\s*Período de Apuração1\s",
+                'cnpj_pagamento_origem_credito': r"ORIGEM DO CRÉDITO.*?(\d{2}\.\d{3}\.\d{3}/\d{4}-\d{2})\s+CNPJ do Pagamento",
+                'codigo_receita_origem_credito': r"ORIGEM DO CRÉDITO.*?Código da Receita\s+(\d{4})",
+                'grupo_tributo_origem_credito': r"ORIGEM DO CRÉDITO.*?Grupo de Tributo\s+([A-Z]+(?:/[A-Z]+)?(?:,\s[A-Z]+)*)",
+                'data_arrecadacao_origem_credito': r"ORIGEM DO CRÉDITO.*?Data da Arrecadação\s+(\d{2}/\d{2}/\d{4})",
+                'valor_principal_origem_credito': r"ORIGEM DO CRÉDITO.*?Valor do Principal\s+([\d.,]+)",
+                'valor_multa_origem_credito': r"ORIGEM DO CRÉDITO.*?Valor da Multa\s+([\d.,]+)",   
+                'valor_juros_origem_credito': r"ORIGEM DO CRÉDITO.*?Valor dos Juros\s+([\d.,]+)",
+                'valor_total_origem_credito': r"ORIGEM DO CRÉDITO.*?Valor Total\s+([\d.,]+)",
             }
         }
 
